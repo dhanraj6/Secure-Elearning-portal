543195061e6f1d7bd353db2412f18e4128e8bb5b
nagavinay336 <nagavinay336@gmail.com>
2021-05-12 18:23:06 +0530

    completed contact us page

--- mf/src/generalPages/contactUs.js
+++ mf/src/generalPages/contactUs.js
@@ -8,27 +8,28 @@ const ContactUs = () => {
             <div className="container">
                 <h6 style={{ color: 'white' }}>
                     <div className="container">
-                        <form id="contact-form">
+                        <form id="contact-form" action="https://formspree.io/f/mqkwpebr" method="post">
 
                             <div className="form-group">
                                 <label htmlFor="name">Name</label>
-                                <input type="text" className="form-control" />
+                                <input type="text" className="form-control" name="name" id="name"/>
                             </div>
 
                             <div className="form-group">
                                 <label htmlFor="exampleInputEmail1">Email address</label>
-                                <input type="email" className="form-control" aria-describedby="emailHelp" />
+                                <input type="email" className="form-control" aria-describedby="emailHelp" name="email" id="email"/>
                             </div>
 
                             <div className="form-group">
                                 <label htmlFor="message">Message</label>
-                                <textarea className="form-control" rows="5" />
+                                <textarea className="form-control" rows="5" name="message" id="message" />
                             </div>
 
                             <button type="submit" className="btn btn-primary" style={{borderRadius:"15px"}}>Submit</button>
 
                         </form>
                     </div>
+                    
                 </h6>
             </div>
         </Base>
